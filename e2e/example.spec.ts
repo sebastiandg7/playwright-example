f4714ee2008dfaf78088aa81b5f15c7651621323
Sebastiandg7 <13395979+sebastiandg7@users.noreply.github.com>
2022-09-21 10:01:28 -0500

    first commit

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,30 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Addi Cupo Widget', () => {
-  test.beforeEach(async ({ page }) => {
-    // Go to the starting url before each test.
-    await page.goto("https://addi.com/");
-  });
+test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
-  test("main navigation", async ({ page }) => {
-    // Assertions use the expect API.
-    await expect(page).toHaveURL("https://co.addi.com/");
-  });
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
 
-  test("cupo widget renders", async ({ page }) => {
-    
-    // Click text=Iniciar Sesión
-  await page.locator('text=Iniciar Sesión').click();
-  // Click input[type="text"]
-  await page.locator('input[type="text"]').click();
-  // Fill input[type="text"]
-  await page.locator('input[type="text"]').fill('988 777 6666');
-  // Click text=Ingresar y ver mi cupo
-  await page.locator('text=Ingresar y ver mi cupo').click();
+  // create a locator
+  const getStarted = page.locator('text=Get Started');
 
-  const noClient = await page.locator('text=Parece que no eres cliente de Addi con el numero 988 777 6666');
-  expect(noClient).toBeTruthy();
-  });
+  // Expect an attribute "to be strictly equal" to the value.
+  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
 
+  // Click the get started link.
+  await getStarted.click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
 });
-
